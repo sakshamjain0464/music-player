61de0c992d2ffe668d2d24c9e41a857cce039ebb
saksham <sakshamjain0464@gmail.com>
2023-09-12 09:49:36 +0530

    Completed Now Playing Section!

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ let song_list = document.querySelector('.song-list')
 function show_scrollbar(){
     song_list.classList.add('scroll-visible')
 }
+
+function hide_scrollbar(){
+    song_list.classList.remove('scroll-visible')
+}
